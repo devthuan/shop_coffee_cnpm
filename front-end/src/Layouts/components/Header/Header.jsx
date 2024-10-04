2f5c9cd82e6fba1276ddf98802d682fffe046f79
MT <minhtruc25012004@gmail.com>
2024-10-04 21:31:42 +0700

    input search

--- front-end/src/Layouts/components/Header/Header.jsx
+++ front-end/src/Layouts/components/Header/Header.jsx
@@ -3,36 +3,45 @@ import styles from "./Header.module.scss";
 import logo from "~/assets/images/logo.svg";
 import ThemeToggle from "~/Layouts/components/ThemeToggle";
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass, faCartShopping, faUser, faBars } from "@fortawesome/free-solid-svg-icons";
 import SideBarHeader from "~/Layouts/components/Header/SideBarHeader/SideBarHeader";
+import InputSearch from "~/Layouts/components/Header/InputSearch/inputSearch";
+
 
 const cx = classNames.bind(styles);
 function Header() {
   const [Login, setLogin] = useState(false)
   const [imgUser, setImgUser] = useState()
-  const [sideBarHeader, setSideBarHeader ] = useState(false)
+  const [sideBarHeader, setSideBarHeader] = useState(false)
+  const [toggleSearch, setToggleSearch] = useState(false)
+
+
+  const handleCLickSearch = () =>{
+    setToggleSearch(!toggleSearch)
+  }
+
   const handleClickToggle = () => {
-      setSideBarHeader(!sideBarHeader)
+    setSideBarHeader(!sideBarHeader)
   }
   return (
     <header className={cx("wrapper", "sm:h-[110px] h-[82px]")}>
-       {sideBarHeader && <div className={cx("sidebarheader")}><SideBarHeader handleClickToggle ={handleClickToggle}/></div>}
+      {sideBarHeader && <div className={cx("sidebarheader")}><SideBarHeader handleClickToggle={handleClickToggle} /></div>}
+      {toggleSearch && <InputSearch handleCLickSearch={handleCLickSearch} />}
       <div className={cx("container")}>
-        <FontAwesomeIcon icon={faBars} className={cx("icon_bars", "sm:hidden")} onClick={handleClickToggle}/>
+        <FontAwesomeIcon icon={faBars} className={cx("icon_bars", "sm:hidden")} onClick={handleClickToggle} />
 
         <Link to={'/'} className={cx("logo")}>
           <img src={logo} alt="Logo" className={cx("logo_icon")} />
           <div className={cx("title")}>grocerymart</div>
         </Link>
-
         <div className={cx("element_right")}>
           {Login ? <div className={cx("header_btn")}>
             <Link to="./register" className={cx("btn_login_text")}>log in</Link>
             <ThemeToggle />
           </div> : <div className={cx("content")} >
-            <div className={cx("btn_search")}>
+            <div className={cx("btn_search")} onClick={handleCLickSearch}>
               <FontAwesomeIcon icon={faMagnifyingGlass} />
             </div>
             <Link to={'/cart'} className={cx("btn_cart")}>
