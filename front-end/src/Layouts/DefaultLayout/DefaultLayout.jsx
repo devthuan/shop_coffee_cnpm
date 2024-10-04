79a3ff9e81d19af323bdf28410e91b23af355cda
MT <minhtruc25012004@gmail.com>
2024-10-04 12:18:24 +0700

    reponsive header

--- front-end/src/Layouts/DefaultLayout/DefaultLayout.jsx
+++ front-end/src/Layouts/DefaultLayout/DefaultLayout.jsx
@@ -1,12 +1,13 @@
 import classNames from "classnames/bind";
 import styles from "./DefaultLayout.module.scss";
+import Header from "~/Layouts/components/Header/Header";
 
 const cx = classNames.bind(styles);
 
 function DefaultLayout({children}) {
   return (
     <div className={cx(" max-w-[1280px] mx-auto max-sm:p-5 text-center")}>
-      <div className="grid grid-cols-1">header</div>
+      <div className="grid grid-cols-1"><Header/></div>
       <div className={cx("lg:w-11/12 grid grid-cols-12 gap-x-7  mx-auto")}>
         <div className={cx("lg:col-span-3 max-sm:col-span-12 ")}>sidebar</div>
         <div className={cx("lg:col-span-9 max-sm:col-span-12")}>
--- front-end/src/Layouts/components/Header/Header.jsx
+++ front-end/src/Layouts/components/Header/Header.jsx
@@ -5,37 +5,52 @@ import ThemeToggle from "~/Layouts/components/ThemeToggle";
 import { Link } from "react-router-dom";
 import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMagnifyingGlass, faCartShopping, faUser } from "@fortawesome/free-solid-svg-icons";
+import { faMagnifyingGlass, faCartShopping, faUser, faBars } from "@fortawesome/free-solid-svg-icons";
+import SideBarHeader from "~/Layouts/components/Header/SideBarHeader/SideBarHeader";
 
 const cx = classNames.bind(styles);
 function Header() {
   const [Login, setLogin] = useState(false)
+  const [imgUser, setImgUser] = useState()
+  const [sideBarHeader, setSideBarHeader ] = useState(false)
+  const handleClickToggle = () => {
+      setSideBarHeader(!sideBarHeader)
+  }
   return (
-    <header className={cx("wrapper")}>
+    <header className={cx("wrapper", "sm:h-[110px] h-[82px]")}>
+       {sideBarHeader && <div className={cx("sidebarheader")}><SideBarHeader handleClickToggle ={handleClickToggle}/></div>}
       <div className={cx("container")}>
+        <FontAwesomeIcon icon={faBars} className={cx("icon_bars", "sm:hidden")} onClick={handleClickToggle}/>
+
         <Link to={'/'} className={cx("logo")}>
           <img src={logo} alt="Logo" className={cx("logo_icon")} />
           <div className={cx("title")}>grocerymart</div>
         </Link>
-        {Login ? <div className={cx("header_btn")}>
-          <Link to="./register" className={cx("btn_login_text")}>log in</Link>
-          <ThemeToggle />
-        </div> : <div className={cx("content")} >
-          <div className={cx("btn_search")}>
-            <FontAwesomeIcon icon={faMagnifyingGlass} />
-          </div>
-          <Link to={'/cart'} className={cx("btn_cart")}>
-            <FontAwesomeIcon icon={faCartShopping} />
-          </Link>
-          <Link to={'/profile'} className={cx("img_user")}>
-            <FontAwesomeIcon icon={faUser} />
-          </Link>
-          <ThemeToggle />
-        </div>}
 
+        <div className={cx("element_right")}>
+          {Login ? <div className={cx("header_btn")}>
+            <Link to="./register" className={cx("btn_login_text")}>log in</Link>
+            <ThemeToggle />
+          </div> : <div className={cx("content")} >
+            <div className={cx("btn_search")}>
+              <FontAwesomeIcon icon={faMagnifyingGlass} />
+            </div>
+            <Link to={'/cart'} className={cx("btn_cart")}>
+              <FontAwesomeIcon icon={faCartShopping} />
+            </Link>
+
+            <Link to={'/profile'}>
+              {imgUser ? <img src={imgUser} /> : <FontAwesomeIcon icon={faUser} className={cx("img_user")} />}
+            </Link>
+            <ThemeToggle />
+          </div>}
+        </div>
+
+        <Link to={'/profile'} className={cx("sm:hidden")}>
+          <FontAwesomeIcon icon={faUser} className={cx("img_user")} />
+        </Link>
       </div>
     </header>
   );
 }
-
 export default Header;
