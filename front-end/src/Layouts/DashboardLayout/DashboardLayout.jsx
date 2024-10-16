aeb3ab2f982572123c0f273b7c372ada6b4248ce
thuan <devthuan24@gmail.com>
2024-10-17 00:31:25 +0700

    create base ui admin

---
+++ front-end/src/Layouts/DashboardLayout/DashboardLayout.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import classNames from "classnames/bind";
+import styles from "./DashboardLayout.module.scss";
+import Sidebar from "../components/Sidebar/Sidebar";
+import HeaderAdmin from "../components/HeaderAdmin/HeaderAdmin";
+const cx = classNames.bind(styles);
+function DashboardLayout({ children }) {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  return (
+    <div className="grid grid-cols-12 gap-x-5">
+      <div className="h-screen col-span-2">
+        <Sidebar />
+      </div>
+      <div className="col-span-10">
+        <HeaderAdmin/>
+        <div className="">content</div>
+      </div>
+    </div>
+  );
+}
+
+export default DashboardLayout;
--- front-end/src/router/adminRoutes.jsx
+++ front-end/src/router/adminRoutes.jsx
@@ -1,6 +1,8 @@
-
+import DashboardLayout from "~/Layouts/DashboardLayout/DashboardLayout";
+import { Home } from "~/page/admin/Home/Home";
 
 //  routes for admin
 export const adminRoutes = [
+      { path: "/admin", component: Home, layout: DashboardLayout },
 
 ];
