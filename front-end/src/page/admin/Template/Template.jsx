733061317bf24aad42a020f6b2d71de4ed7121d1
thuan <devthuan24@gmail.com>
2024-10-23 23:29:06 +0700

    create page layout template

--- front-end/src/page/admin/Template/Template.jsx
+++ front-end/src/page/admin/Template/Template.jsx
@@ -106,10 +106,8 @@ export const Template = () => {
 
         {/* box button create */}
         <div className="mt-3 md:mt-0">
+          {/* import modal create */}
           <TemplateModelCreate />
-          {/* <p className="inline-block px-4 py-2 text-white duration-150 font-medium bg-indigo-600 rounded-lg hover:bg-indigo-500 active:bg-indigo-700 md:text-sm">
-            Create
-          </p> */}
         </div>
       </div>
 
@@ -162,14 +160,14 @@ export const Template = () => {
           </tbody>
         </table>
       </div>
-          <Pagination
-            totalItems={1} // truyền các trị tương ứng với biến
-            current={1} // truyền các trị tương ứng với biến
-            totalPage={1} // truyền các trị tương ứng với biến
-            limit={1} // truyền các trị tương ứng với biến
-            onPageChange={1} // truyền các trị tương ứng với biến
-            onLimitChange={1} // truyền các trị tương ứng với biến
-          />
+      <Pagination
+        totalItems={1} // truyền các trị tương ứng với biến
+        current={1} // truyền các trị tương ứng với biến
+        totalPage={1} // truyền các trị tương ứng với biến
+        limit={1} // truyền các trị tương ứng với biến
+        onPageChange={1} // truyền các trị tương ứng với biến
+        onLimitChange={1} // truyền các trị tương ứng với biến
+      />
     </div>
   );
 };
