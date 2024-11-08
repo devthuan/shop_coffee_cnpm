190033ec40e86745ff26ba15729ae8c0b4141c57
MT <minhtruc25012004@gmail.com>
2024-11-08 19:14:17 +0700

    update user

--- front-end/src/Layouts/ProfileLayout/ProfileSidebar/profileSidebar.jsx
+++ front-end/src/Layouts/ProfileLayout/ProfileSidebar/profileSidebar.jsx
@@ -1,14 +1,15 @@
 import classNames from "classnames/bind";
 import styles from "./ProfileSidebar.module.scss"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCircle, faCircleExclamation, faCircleInfo, faDownload, faEnvelope, faGift, faHeart, faInfo, faLocation, faLocationDot, faSchoolCircleExclamation, faShield, faShieldHalved, faUser } from "@fortawesome/free-solid-svg-icons";
+import { faCartPlus, faCircle, faCircleExclamation, faCircleInfo, faDownload, faEnvelope, faGift, faHeart, faInfo, faLocation, faLocationDot, faSchoolCircleExclamation, faShield, faShieldHalved, faUser } from "@fortawesome/free-solid-svg-icons";
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const cx = classNames.bind(styles)
 
 function ProfileSidebar() {
 
-    const [imgUser, setImgUser] = []
+   
     const id = useSelector((state) => state.userInfo.id)
     const createdAt = useSelector((state) => state.userInfo.createdAt)
     const updatedAt = useSelector((state) => state.userInfo.updatedAt)
@@ -27,10 +28,10 @@ function ProfileSidebar() {
     return (<div className={cx("wrapper")}>
         <div className={cx("avatar")}>
             <div className={cx("avatar_content")}>
-                {imgUser ? <img src={imgUser} className={cx("img_user")} /> : <FontAwesomeIcon icon={faUser} className={cx("img_user")} />}
-                <div className={cx("name_user")}>{userName}</div>
+                {userInformation?.avatar ? <img src={userInformation?.avatar} className={cx("img_user")} /> : <FontAwesomeIcon icon={faUser} className={cx("img_user")} />}
+                <div className={cx("name_user")}>{userInformation?.fullName}</div>
                 <div className={cx("register_login")}>
-                    <div className={cx("register_login_text")}>Registered:</div>
+                    <div className={cx("register_login_text")}>ngày đăng kí:</div>
                     <div className={cx("register_login_date")}>{createdAt}</div>
                 </div>
             </div>
@@ -39,60 +40,51 @@ function ProfileSidebar() {
             <ul className={cx("list_container")}>
                 <li className={cx("list_item")}>
                     <ul className={cx("list_item_container")}>
-                        Manage Account
+                        quản lí
+                        <Link to="/profile/editProfile">
                         <li className={cx("list_item_item")}>
                             <FontAwesomeIcon icon={faUser} className={cx("item_icon")} />
-                            <div className={cx("item_text")}>Personal info</div>
+                            <div className={cx("item_text")}>chỉnh sửa thông tin cá nhân</div>
                         </li>
-                        <li className={cx("list_item_item")}>
-                            <FontAwesomeIcon icon={faLocationDot} className={cx("item_icon")} />
-                            <div className={cx("item_text")}>Addresses</div>
-                        </li>
-                        <li className={cx("list_item_item")}>
-                            <FontAwesomeIcon icon={faEnvelope} className={cx("item_icon")} />
-                            <div className={cx("item_text")}>Communications & privacy</div>
-                        </li>
+                        </Link>
                     </ul>
                 </li>
 
                 <li className={cx("list_item")}>
                     <ul className={cx("list_item_container")}>
-                        My items
-                        <li className={cx("list_item_item")}>
-                            <FontAwesomeIcon icon={faDownload} className={cx("item_icon")} />
-                            <div className={cx("item_text")}>Personal info</div>
-                        </li>
+                        Các mục của tôi
+                        
                         <li className={cx("list_item_item")}>
                             <FontAwesomeIcon icon={faHeart} className={cx("item_icon")} />
-                            <div className={cx("item_text")}> Lists</div>
+                            <div className={cx("item_text")}>yêu thích</div>
                         </li>
                         <li className={cx("list_item_item")}>
-                            <FontAwesomeIcon icon={faGift} className={cx("item_icon")} />
-                            <div className={cx("item_text")}>Registries</div>
+                            <FontAwesomeIcon icon={faCartPlus} className={cx("item_icon")} />
+                            <div className={cx("item_text")}>đã mua</div>
                         </li>
                     </ul>
                 </li>
 
                 <li className={cx("list_item")}>
                     <ul className={cx("list_item_container")}>
-                        Subscriptions & plans
+                       hỗ trợ
                         <li className={cx("list_item_item")}>
                             <FontAwesomeIcon icon={faShieldHalved} className={cx("item_icon")} />
-                            <div className={cx("item_text")}>Protection plans</div>
+                            <div className={cx("item_text")}>bảo vệ</div>
                         </li>
                     </ul>
                 </li>
 
                 <li className={cx("list_item")}>
                     <ul className={cx("list_item_container")}>
-                        Customer Service
+                        Dịch vụ
                         <li className={cx("list_item_item")}>
                             <FontAwesomeIcon icon={faCircleInfo} className={cx("item_icon")} />
-                            <div className={cx("item_text")}>Help</div>
+                            <div className={cx("item_text")}>Giúp</div>
                         </li>
                         <li className={cx("list_item_item")}>
                             <FontAwesomeIcon icon={faCircleExclamation} className={cx("item_icon")} />
-                            <div className={cx("item_text")}>Terms of Use</div>
+                            <div className={cx("item_text")}>Điều khoản</div>
                         </li>
                     </ul>
                 </li>
@@ -100,5 +92,4 @@ function ProfileSidebar() {
         </div>
     </div>);
 }
-
 export default ProfileSidebar;
--- front-end/src/page/admin/Bill/Bill.jsx
+++ front-end/src/page/admin/Bill/Bill.jsx
@@ -8,7 +8,6 @@ import { clearDataBill, initDataBill } from "~/redux/features/Bill/billSilice";
 import { useDispatch } from "react-redux";
 import { HandleApiError } from "~/Utils/HandleApiError";
 import { toast, ToastContainer } from "react-toastify";
-import ModelEditBill from "~/page/admin/Bill/ModelEditBills";
 import { Pagination } from "~/components/Pagination/Pagination";
 import BillDetails from "~/page/admin/Bill/BillDetails";
 const cx = classNames.bind(styles);
@@ -135,35 +134,7 @@ export const Bill = () => {
       ) : (
         <div className="mx-auto  md:pr-5">
           <div className=" ">
-            {/* <div className="flex justify-center ">
-              <form
-                onSubmit={(e) => e.preventDefault()}
-                className="max-w-xl w-full px-4 mx-auto mt-5"
-              >
-                <div className="relative">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="absolute top-0 bottom-0 w-6 h-6 my-auto text-gray-400 left-3"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth={2}
-                      d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                    />
-                  </svg>
-                  <input
-                    onChange={(e) => handleSearch(e.target.value)}
-                    type="text"
-                    placeholder="Search"
-                    className="w-full py-1 pl-12 pr-4 text-[18px] text-gray-500 border rounded-md outline-none bg-gray-50 focus:bg-white focus:border-indigo-600"
-                  />
-                </div>
-              </form>
-            </div> */}
+            
             <div className="flex justify-between mt-7">
               <div className="relative w-72 ">
                 <svg
@@ -195,9 +166,7 @@ export const Bill = () => {
                     })}
                 </select>
               </div>
-              <div className="">
-                {/* <ModelAddAccount /> */}
-              </div>
+             
             </div>
           </div>
           {isLoading ? (
@@ -225,7 +194,7 @@ export const Bill = () => {
                   {BillsData?.map((item, idx) => (
                     <tr key={idx}>
                       <td className="px-2 py-4 whitespace-nowrap w-[20px]  hover:text-blue-500 hover:underline cursor-pointer" onClick={() => handleClickIDBill(item.id)}>
-                        {item.id}
+                      {item.id.slice(0, 8)} ...
                       </td>
                       <td className="px-2 py-4 whitespace-nowrap">
                         {item.fullName}
@@ -263,19 +232,13 @@ export const Bill = () => {
                         {item.note}
                       </td>
 
-                      <td className="px-2 py-4 whitespace-nowrap">
-                        <div>
-                          <ModelEditBill data={item} />
-                        </div>
-                      </td>
+                     
                     </tr>
                   ))}
                 </tbody>
               </table>
             </div>
           )}
-
-         
           <Pagination
             totalItems={total}
             current={currentPage}
